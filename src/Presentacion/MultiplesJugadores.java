e6bae15d8f0ab1da8aadd18ee3125bfcef505ab3
brunosagrera <bsagrera7@gmail.com>
2021-03-26 11:40:42 +0100

    Commit Bruno con clases creadas y MultiJugador en proceso

--- src/Presentacion/MultiplesJugadores.java
+++ src/Presentacion/MultiplesJugadores.java
@@ -11,15 +11,19 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JComboBox;
 
 public class MultiplesJugadores extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textFieldJugador1;
 	private JTextField textFieldJugador2;
+	private static int CPU=0;
 
 	/**
 	 * Launch the application.
@@ -51,23 +55,66 @@ public class MultiplesJugadores extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnJUGADOR1 = new JButton("JUGADOR 1");
-		btnJUGADOR1.setBounds(62, 85, 128, 48);
-		contentPane.add(btnJUGADOR1);
-		
-		JButton btnJUGADOR2 = new JButton("JUGADOR 2");
-		btnJUGADOR2.setBounds(62, 173, 128, 48);
-		contentPane.add(btnJUGADOR2);
+		JButton btnJugador1 = new JButton("Jugador");
+		btnJugador1.setBounds(62,85,128,48);
+		contentPane.add(btnJugador1);
 		
 		textFieldJugador1 = new JTextField();
 		textFieldJugador1.setBounds(263, 86, 253, 47);
 		contentPane.add(textFieldJugador1);
 		textFieldJugador1.setColumns(10);
-		
-		textFieldJugador2 = new JTextField();
-		textFieldJugador2.setBounds(263, 174, 253, 47);
-		contentPane.add(textFieldJugador2);
-		textFieldJugador2.setColumns(10);
+//		JComboBox comboJuagdor1 = new JComboBox();
+//		comboJuagdor1.setBounds(62, 85, 128, 48);
+//		comboJuagdor1.addItem("Jugador");
+//		comboJuagdor1.addItem("CPU");
+//        contentPane.add(comboJuagdor1);
+//		textFieldJugador1 = new JTextField();
+//		textFieldJugador1.setBounds(263, 86, 253, 47);
+//		contentPane.add(textFieldJugador1);
+//		textFieldJugador1.setColumns(10);
+//        comboJuagdor1.addItemListener(new ItemListener() {
+//		    public void itemStateChanged(ItemEvent arg0) {
+//		    	String itemSeleccionado1 = (String)comboJuagdor1.getSelectedItem();
+//		    	if ("Jugador".equals(itemSeleccionado1)) {
+//		            //Creamos una instancia de Humano donde recogeremos del inputtext el Username.
+//		    		textFieldJugador1.setText("");
+//		         	textFieldJugador1.setEditable(true);
+//		         	CPU--;
+//		         }else{
+//		         	//Creamos una instancia de CPU donde recogeremos del inputtext .
+//		         	textFieldJugador1.setText("Al"+CPU);
+//		         	textFieldJugador1.setEditable(false);
+//		         	CPU++;
+//		         	}
+//		    }
+//        });
+        
+        
+        JComboBox comboJuagdor1 = new JComboBox();
+        comboJuagdor1.setBounds(62, 143, 128, 48);
+        comboJuagdor1.addItem("Jugador");
+        comboJuagdor1.addItem("CPU");
+        contentPane.add(comboJuagdor1);
+        textFieldJugador1 = new JTextField();
+		textFieldJugador1.setBounds(263, 143, 253, 47);
+		contentPane.add(textFieldJugador1);
+		textFieldJugador1.setColumns(10);
+		comboJuagdor1.addItemListener(new ItemListener() {
+		    public void itemStateChanged(ItemEvent arg0) {
+		    	String itemSeleccionado1 = (String)comboJuagdor1.getSelectedItem();
+		    	if ("Jugador".equals(itemSeleccionado1)) {
+		            //Creamos una instancia de Humano donde recogeremos del inputtext el Username.
+		    		textFieldJugador1.setText("");
+		         	textFieldJugador1.setEditable(true);
+		         	CPU--;
+		         }else{
+		         	//Creamos una instancia de CPU donde recogeremos del inputtext .
+		         	textFieldJugador1.setText("Al"+CPU);
+		         	textFieldJugador1.setEditable(false);
+		         	CPU++;
+		         	}
+		    }
+		});
 		
 		JButton btnJugar = new JButton("JUGAR");
 		btnJugar.addActionListener(new ActionListener() {
@@ -82,16 +129,21 @@ public class MultiplesJugadores extends JFrame {
 		btnJugar.setBounds(608, 117, 157, 68);
 		contentPane.add(btnJugar);
 		
-		JButton btnañadir = new JButton("+");
-		btnañadir.setBackground(Color.GREEN);
-		btnañadir.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnañadir.addActionListener(new ActionListener() {
+		JButton btnanadir = new JButton("+");
+		btnanadir.setBackground(Color.BLUE);
+		btnanadir.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnanadir.setBounds(608, 400, 70, 47);
+		contentPane.add(btnanadir);
+		btnanadir.addActionListener(new ActionListener() {
+		int ultimaposiciony=173, i=3; 
 			public void actionPerformed(ActionEvent e) {
+				JButton BtnJugador = new JButton("JUGADOR "+i);
+				i++;
+				ultimaposiciony+= 60;
+				BtnJugador.setBounds(62, ultimaposiciony, 128, 48);
+				contentPane.add(BtnJugador);
 			}
 		});
-		
-		btnañadir.setBounds(366, 260, 70, 47);
-		contentPane.add(btnañadir);
 		JLabel background;
         ImageIcon img = new ImageIcon("resources/FondoJugadoresvs.jpg");
         background = new JLabel("", img, JLabel.CENTER);
