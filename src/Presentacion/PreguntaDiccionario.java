018c02da7611a77560f19092b3e234042e3c63bc
Andrés Gassó <a19andgasgas@inspedralbes.cat>
2021-04-06 09:50:31 +0200

    Preguntas Diccionario -Realizar cambios

--- src/Presentacion/PreguntaDiccionario.java
+++ src/Presentacion/PreguntaDiccionario.java
@@ -1,9 +1,16 @@
 package Presentacion;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -32,11 +39,44 @@ public class PreguntaDiccionario extends JFrame {
 	 */
 	public PreguntaDiccionario() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo(null);
+		setSize(1000,600);
+		//setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel background;
+        ImageIcon img = new ImageIcon("resources/fondoDuracionPartida3.jpg");
+        background = new JLabel("", img, JLabel.CENTER);
+        background.setBounds(0, 0, 1000, 600);
+        contentPane.add(background);
+        JButton btnCHECK = new JButton("CHECK");
+		btnCHECK.setForeground(Color.BLACK);
+		btnCHECK.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnCHECK.setBackground(Color.YELLOW);
+		btnCHECK.setBounds(248, 369, 99, 38);
+		background.add(btnCHECK);
+		
+		//Palabra ejemplo ABOMINABLEMENTE (15) A_O_INA_B_LEM_T_ (tapar 15/3=5)
+		
+		
+		
+		JButton btnNEXT = new JButton("NEXT");
+		btnNEXT.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PreguntaMates pM= new PreguntaMates();
+				pM.setVisible(true);
+				PreguntaDiccionario.this.dispose();
+			}
+		});
+		btnNEXT.setForeground(Color.WHITE);
+		btnNEXT.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnNEXT.setBackground(Color.BLUE);
+		btnNEXT.setBounds(433, 369, 99, 38);
+		background.add(btnNEXT);
 	}
+	
 
 }
