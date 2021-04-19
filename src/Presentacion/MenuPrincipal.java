60c64aaeb9b6abc63785274a3ee4ea231813c04f
brunosagrera <bsagrera7@gmail.com>
2021-04-19 12:14:49 +0200

    Jugadors en proceso

--- src/Presentacion/MenuPrincipal.java
+++ src/Presentacion/MenuPrincipal.java
@@ -7,6 +7,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 
+import Logica.Jugador;
 import Logica.StyledButtonUI;
 
 import java.awt.event.ActionListener;
@@ -19,6 +20,7 @@ public class MenuPrincipal extends JPanel {
 	 * Create the panel.
 	 */
 	StartSlide ventanaInicio;
+	Jugador jugador;
 	
 	public MenuPrincipal(StartSlide ventanaInicio) {
 		this.ventanaInicio = ventanaInicio;
@@ -34,18 +36,28 @@ public class MenuPrincipal extends JPanel {
 		btnInforme.setUI(new StyledButtonUI());
 		add(btnInforme);
 		
+		
+		JButton btnJugadores = new JButton("JUGADORS");
+		btnJugadores.setBounds(400, 366, 191, 41);
+		btnJugadores.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnJugadores.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Jugadors jugadors = new Jugadors(jugador);
+				jugadors.setVisible(true);
+				ventanaInicio.dispose();
+			}
+		});
+		btnJugadores.setUI(new StyledButtonUI());
+		add(btnJugadores);
+		
 		JButton btnOpciones = new JButton("OPCIONES");
 		btnOpciones.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//prueba
-				
-				
-				
 				ventanaInicio.dispose();
-				
 			}
 		});
-		btnOpciones.setBounds(400, 366, 191, 41);
+		btnOpciones.setBounds(400, 441, 191, 41);
 		btnOpciones.setFont(new Font("Tahoma", Font.BOLD, 18));
 		btnOpciones.setUI(new StyledButtonUI());
 		add(btnOpciones);
